7c049eaeb9be21a030ebaccab1fc54a2c0abca75
srishti-maurya <srishti22maurya@gmail.com>
2022-01-10 14:39:16 +0530

    converted to ES6

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
+let inputTranslate = document.querySelector("#translate-input");
+let btnTranslate = document.querySelector("#translate-btn");
+let outputTranslate = document.querySelector("#translate-output");
+let UrlServer = "https://api.funtranslations.com/translate/yoda.json";
 
-var inputTranslate = document.querySelector("#translate-input");
-var btnTranslate = document.querySelector("#translate-btn");
-var outputTranslate = document.querySelector("#translate-output");
-var UrlServer = "https://api.funtranslations.com/translate/yoda.json"
+const buildURL = (text) => UrlServer + "?" + "text=" + text;
 
-function buildURL(text){
-    return UrlServer+"?"+"text="+text
- }
- 
-function handleError(err){
-    console.log("an error occured",err);
-    alert("There is something wrong with the server! Please try again later")
-}
+const handleError = (err) => {
+  console.log("an error occured", err);
+  alert("There is something wrong with the server! Please try again later");
+};
 
-function clickHandler() {
-  var textInput = inputTranslate.value;
+const clickHandler = () => {
+  let textInput = inputTranslate.value;
   fetch(buildURL(textInput))
-    .then(res=>res.json())
-    .then(json =>{
-        var translatedText = json.contents.translated;
-        outputTranslate.innerHTML = translatedText;
+    .then((res) => res.json())
+    .then((json) => {
+      let translatedText = json.contents.translated;
+      outputTranslate.innerHTML = translatedText;
     })
-    .catch(handleError)
-}
+    .catch(handleError);
+};
 
 btnTranslate.addEventListener("click", clickHandler);
-
